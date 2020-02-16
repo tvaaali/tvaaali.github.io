1818e0121bd9b8c0402f6c2af3a302bc1bf63d84
Vali TB <tvaaali@gmail.com>
2020-02-16 21:42:25 +0200

    +

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
 }
+
+
+  function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("z-include-html");
+      if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+              elmnt.innerHTML = this.responseText;
+            }
+            if (this.status == 404) {
+              elmnt.innerHTML = "Page not found.";
+            }
+            elmnt.removeAttribute("z-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+  }
