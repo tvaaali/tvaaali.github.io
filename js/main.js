37cacc31870697b1d62a74bc00cc29255ba1b83c
Vali TB <tvaaali@gmail.com>
2020-02-16 22:33:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,30 +8,31 @@ if ('serviceWorker' in navigator) {
   });
 }
 
- $("#zcontent").load("/included.html");
-  // (function includeHTML() {
-  //   var z, i, elmnt, file, xhttp;
-  //   z = document.getElementsByTagName("*");
-  //   for (i = 0; i < z.length; i++) {
-  //     elmnt = z[i];
-  //     file = elmnt.getAttribute("z-include-html");
-  //     if (file) {
-  //       xhttp = new XMLHttpRequest();
-  //       xhttp.onreadystatechange = function () {
-  //         if (this.readyState == 4) {
-  //           if (this.status == 200) {
-  //             elmnt.innerHTML = this.responseText;
-  //           }
-  //           if (this.status == 404) {
-  //             elmnt.innerHTML = "Page not found.";
-  //           }
-  //           elmnt.removeAttribute("z-include-html");
-  //           includeHTML();
-  //         }
-  //       }
-  //       xhttp.open("GET", file, true);
-  //       xhttp.send();
-  //       return;
-  //     }
-  //   }
-  // })();
+//  $("#zcontent").load("/included.html");
+  
+  (function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("z-include-html");
+      if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+              elmnt.innerHTML = this.responseText;
+            }
+            if (this.status == 404) {
+              elmnt.innerHTML = "Page not found.";
+            }
+            elmnt.removeAttribute("z-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+  })();
