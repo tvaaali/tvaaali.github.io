e1e756515692247990e3f91268130cb0c8cc4d14
Vali TB <tvaaali@gmail.com>
2020-02-16 21:21:39 +0200

    Update sw_cached_site.js

--- sw/sw_cached_site.js
+++ sw/sw_cached_site.js
@@ -1,13 +1,13 @@
-const cacheName = 'v2';
+const cacheName = 'v4';
 
 // Call Install Event
 self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
+  console.log('Service Worker: Installed', e);
 });
 
 // Call Activate Event
 self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
+  console.log('Service Worker: Activated', e);
   // Remove unwanted caches
   e.waitUntil(
     caches.keys().then(cacheNames => {
@@ -25,15 +25,17 @@ self.addEventListener('activate', e => {
 
 // Call Fetch Event
 self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching');
+  console.log('Service Worker: Fetching: ', e);
   e.respondWith(
     fetch(e.request)
       .then(res => {
         // Make copy/clone of response
         const resClone = res.clone();
+        console.log("The clone: ", resclone)
         // Open cahce
         caches.open(cacheName).then(cache => {
           // Add response to cache
+          console.log("Put Cache:", cache);
           cache.put(e.request, resClone);
         });
         return res;
